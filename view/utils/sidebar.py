b4d54e912b77dc950154570e7300da78c982c185
LuannMateus <luanmateuslima34@gmail.com>
2024-06-04 20:47:31 -0300

    refactor: adjust interface

--- view/utils/sidebar.py
+++ view/utils/sidebar.py
@@ -4,9 +4,11 @@ def DefaultSidebar(st):
 
     no_sidebar_style = """
         <style>
-            ul [data-testid="stSidebarNavItems" class="st-emotion-cache-1s0bj5q eczjsme9"] {
+            ul [data-testid="stSidebarNavItems" class="st-emotion-cache-1s0bj5q eczjsme9"],
+            #root > div:nth-child(1) > div.withScreencast > div > div > div > section.st-emotion-cache-1itdyc2.eczjsme16 > div.st-emotion-cache-6qob1r.eczjsme8 > div.st-emotion-cache-79elbk.eczjsme15 > ul {
                 display: none;
             }
+            
 
             #root > div:nth-child(1) > div.withScreencast > div > div > div > section.st-emotion-cache-1gv3huu.eczjsme11 > div.st-emotion-cache-6qob1r.eczjsme3 > div.st-emotion-cache-79elbk.eczjsme10 > ul {
                 display: none;
